3b183cc806bae2ac6e22e6fc225a04541f484c12
EndriDemaj <edemaj22@epoka.edu.al>
2023-09-22 10:38:59 +0200

    test

---
+++ helloEpoka/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for helloEpoka project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helloEpoka.settings')
+
+application = get_asgi_application()
